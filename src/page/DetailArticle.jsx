9cdb759ea7a67b1f465e5b6a5c1944ff05701e00
AryaJulianda <official.aryajulianda@gmail.com>
2023-10-27 17:49:03 +0700

    fix invalid time

--- src/page/DetailArticle.jsx
+++ src/page/DetailArticle.jsx
@@ -32,11 +32,11 @@ const DetailArticle = (props) => {
             <div className="w-[50%] h-80 bg-black rounded-xl bg-cover bg-center flex justify-center items-center" style={{backgroundImage:`url('${article?.img}')`}}>{article.img==null && (<p className="text-white text-lg font-semibold">No Image</p>)}</div>
             <div className="text-right">
               <p className="text-lg font-semibold text-lime-500">{article?.author_name}</p>
-              <p className="text-lg font-semibold text-white">{article && format(new Date(article?.date), 'd MMMM yyyy')}</p>
+              <p className="text-lg font-semibold text-white">{Object.keys(article).length > 0 && format(new Date(article?.date), 'd MMMM yyyy')}</p>
             </div>
           </div>
           <p className="text-lg font-medium text-white">
-            {article && formatContent(article?.content)}
+            {Object.keys(article).length > 0 && formatContent(article?.content)}
           </p>
         </div>
       </div>
